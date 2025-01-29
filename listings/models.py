f3f582156c1026817e08f74e9ae6b53332d198b0
abhayas07 <Abhayabh422@gmail.com>
2025-01-29 16:00:10 +0530

    gg

--- listings/models.py
+++ listings/models.py
@@ -1,29 +1,41 @@
 from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
+from django.contrib.auth.models import User
 
 class Listing(models.Model):
-  realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
-  title = models.CharField(max_length=200)
-  address = models.CharField(max_length=200)
-  city = models.CharField(max_length=100)
-  state = models.CharField(max_length=100)
-  zipcode = models.CharField(max_length=20)
-  description = models.TextField(blank=True)
-  price = models.IntegerField()
-  bedrooms = models.IntegerField()
-  bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
-  garage = models.IntegerField(default=0)
-  sqft = models.IntegerField()
-  lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-  photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
-  photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-  photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-  photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-  photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-  photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-  photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-  is_published = models.BooleanField(default=True)
-  list_date = models.DateTimeField(default=datetime.now, blank=True)
-  def __str__(self):
-    return self.title
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()  # e.g., 1-5 stars
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review for {self.listing.title} by {self.user.username}"
